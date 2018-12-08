222232f392a73b8e9257a574197fda7f732c2466
PaulieB <36047697+Paulieb821@users.noreply.github.com>
2018-12-08 16:32:17 -0500

    Commit die

---
+++ app.js
@@ -0,0 +1,42 @@
+//Setup
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const path = require("path");
+const fs = require("fs");
+
+server.listen(1000);
+
+app.use(express.static(path.join(__dirname, "")));
+app.use((req, res) => res.sendFile(`${__dirname}/index.html`));
+
+//Variables
+var playerList = {};
+var playerArray = [];
+
+//Connection Function
+io.sockets.on("connection", function(socket) {
+  playerList[socket.id] = {health: 5, score: 0};
+  playerArray.push(socket.id);
+  console.log(playerList[socket.id]);
+  socket.emit("connected", playerList.length);
+
+  socket.on("shoot", function(cb) {
+    if(socket == playerList[0]) {
+      playerList[playerArray[1]].health--;
+      cb(playerList[playerArray[0]].score++);
+      socket.broadcast.emit('health', playerList[playerArray[1]].health);
+    }
+    else {
+      playerList[playerArray[0]].health--;
+      cb(playerList[playerArray[1]].score++);
+      socket.broadcast.emit('health', playerList[playerArray[0]].health);
+    }
+  });
+
+  socket.on("disconnect", function() {
+    socket.broadcast.emit("gameEnd");
+    playerList = [];
+  });
+});
---
+++ script.js
@@ -0,0 +1,143 @@
+window.onload = function() {
+  //Definitions
+    const socket = io();
+    var center = {
+      x: 0,
+      y: 0
+    }
+    var targeted = false;
+    var ammo = 10;
+    var hp = 3;
+
+  //Scanner
+    socket.on("connected", function(data) {
+      if(data <= 1) {
+        document.getElementById("player").innerHTML = "Player " + (data + 1);
+      }
+      else {
+        document.getElementById("player").innerHTML = "Spectator";
+      }
+    });
+    socket.on("health", function(data) {
+      document.getElementById("health").innerHTML = data;
+    });
+    var color = {r:4, g:159, b:240};
+    //var color = {r: 3, g: 144, b: 130};
+    var tolerance = 125;
+    var canvas  = document.getElementById('canvas');
+    var context = canvas.getContext('2d');
+    var webcam = document.getElementById('webcam');
+    var vw = 320;
+    var vh = 240;
+
+    var clickTimer;
+
+    // Register our custom color tracking function
+    tracking.ColorTracker.registerColor('dynamic', function(r, g, b) {
+      return getColorDistance(color, {r: r, g: g, b: b}) < tolerance;
+    });
+
+    // Create the color tracking object
+    var tracker = new tracking.ColorTracker("dynamic");
+
+    // Add callback for the "track" event
+    tracker.on('track', function(e) {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      if (e.data.length !== 0) {
+        center.x = e.data[0].x + (e.data[0].width/2);
+        center.y = e.data[0].y + (e.data[0].height/2);
+      }
+      else {
+        center.x = -10;
+        center.y = -10;
+      }
+    });
+
+    // Start tracking
+    tracking.track(webcam, tracker, { camera: true } );
+
+    // Add listener for the click event on the video
+    webcam.addEventListener("click", function (e) {
+
+      // Grab color from the video feed where the click occured
+      var c = getColorAt(webcam, e.offsetX, e.offsetY);
+
+      // Update target color
+      color.r = c.r;
+      color.g = c.g;
+      color.b = c.b;
+
+    });
+
+    // Calculates the Euclidian distance between the target color and the actual color
+    function getColorDistance(target, actual) {
+      return Math.sqrt(
+        (target.r - actual.r) * (target.r - actual.r) +
+        (target.g - actual.g) * (target.g - actual.g) +
+        (target.b - actual.b) * (target.b - actual.b)
+      );
+    }
+
+    // Returns the color at the specified x/y location in the webcam video feed
+    function getColorAt(webcam, x, y) {
+      // To be able to access pixel data from the webcam feed, we must first draw the current frame in
+      // a temporary canvas.
+      var canvas = document.createElement('canvas');
+      var context = canvas.getContext('2d');
+      canvas.width = webcam.width;
+      canvas.height = webcam.height;
+      context.drawImage(webcam, 0, 0, webcam.width, webcam.height);
+
+      // Then we grab the pixel information from the temp canvas and return it as an object
+      var pixel = context.getImageData(x, y, 1, 1).data;
+      return {r: pixel[0], g: pixel[1], b: pixel[2]};
+    }
+
+
+  //Stuff that does things
+    setInterval(function() {
+      if(center.x > vw - 50 && center.x < vw + 50 && center.y > vh - 50 && center.y < vh + 50) {
+        targeted = true;
+        document.getElementById("chsvg").style.color = "red";
+        //console.log("center: (" + center.x + "," + center.y + ")      window center: (" + vw + "," + vh + ")");
+      }
+      else {
+        targeted = false;
+        document.getElementById("chsvg").style.color = "black";
+      }
+    }, 1000/60);
+
+  //Shooting
+    $("body").click(function() {
+      shoot();
+    });
+    $("body").on("tap", function() {
+      shoot();
+    });
+    function shoot() {
+      if(targeted == true) {
+        socket.emit("shoot", function(score) {
+          document.getElementById("score") = score;
+        });
+      }
+      ammo--;
+      document.getElementById("ammo").innerHTML = ammo;
+    }
+
+    document.getElementById("body").addEventListener("mousedown", function() {
+      clickTimer = setTimeout(function() {
+        ammo = 10;
+      }, 5000);
+    });
+    document.getElementById("body").addEventListener("mouseup", function() {
+      clearTimeout(clickTimer);
+    });
+    document.getElementById("body").addEventListener("touchstart", function() {
+      clickTimer = setTimeout(function() {
+        ammo = 10;
+      }, 5000);
+    });
+    document.getElementById("body").addEventListener("touchend", function() {
+      clearTimeout(clickTimer);
+    });
+}
